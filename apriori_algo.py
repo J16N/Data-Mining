0258e8c608000d398f994d29231866c533a49815
Tsubasa <scientistjishan@gmail.com>
2023-01-08 13:03:29 +0530

    Type hinted

--- apriori_algo.py
+++ apriori_algo.py
@@ -1,7 +1,9 @@
 from itertools import count
 from collections import Counter
+from collections.abc import Generator, Iterable
 
-def cartesian_product(items, n_set):
+def cartesian_product(items: Iterable[str], n_set: int) \
+        -> Generator[str, None, None]:
     """
     This function returns the cartesian product
     of the items with itself. The n_set is used 
@@ -22,16 +24,17 @@ def cartesian_product(items, n_set):
     """
     
     for i in range(len(items) - 1):
-        item_group = items[i]
+        item_group: str = items[i]
 
         for j in range(i + 1, len(items)):
-            s = set(item_group).union(items[j])
+            s: set = set(item_group).union(items[j])
 
-            if len(s) == n_set:
+            if (len(s) == n_set):
                 yield ''.join(sorted(s))
 
 
-def get_freq(items, transactions, frequencies):
+def get_freq(items: Iterable[str], transactions: list[set[str]], 
+             frequencies: Counter) -> None:
     for transaction in transactions:
         for item_group in items:
             # if all the item in the item group is in 
@@ -40,19 +43,20 @@ def get_freq(items, transactions, frequencies):
                 frequencies[item_group] += 1
 
 
-def apriori(items, transactions, min_support):
-    L = {}
-    frequencies = Counter()
+def apriori(items: Iterable[str], transactions: list[set[str]], 
+            min_support: float) -> dict:
+    L: dict = {}
+    frequencies: Counter = Counter()
 
     for n_set in count(start=1):
         get_freq(items, transactions, frequencies)
-        support = {
+        support: dict = {
             item_group: frequencies[item_group] / len(transactions)  
             for item_group in items
         }
         # Get items whose value is greater than or 
         # equal to the minimum support
-        L_temp = {
+        L_temp: dict = {
             item_group: support[item_group] 
             for item_group in support 
             if support[item_group] >= min_support
@@ -63,28 +67,29 @@ def apriori(items, transactions, min_support):
         if (not L_temp):
             break
 
-        L = {**L, **L_temp}
-        items = set(cartesian_product(tuple(L_temp.keys()), n_set + 1))
+        L: dict = {**L, **L_temp}
+        items: set = set(cartesian_product(tuple(L_temp.keys()), n_set + 1))
 
     return L
 
 
-def get_rules(itemsets, conf_lvl):
+def get_rules(itemsets: dict, conf_lvl: float) \
+        -> list[tuple[str, str, float]]:
     rules = []
     item_groups = tuple(itemsets.keys())
     N = len(item_groups)
 
     for i in range(N - 1):
-        X = item_groups[i]
+        X: str = item_groups[i]
         
         for j in range(i, N):
-            Y = item_groups[j]
+            Y: str = item_groups[j]
             if (len(set(X) & set(Y)) != 0): continue
-            item_group = ''.join(sorted(set(X) | set(Y)))
+            item_group: str = ''.join(sorted(set(X) | set(Y)))
             if (itemsets.get(item_group) is None): continue
 
-            conf_x_y = itemsets[item_group] / itemsets[X]
-            conf_y_x = itemsets[item_group] / itemsets[Y]
+            conf_x_y: float = itemsets[item_group] / itemsets[X]
+            conf_y_x: float = itemsets[item_group] / itemsets[Y]
 
             if (conf_x_y >= conf_lvl): rules.append((X, Y, conf_x_y))
             if (conf_y_x >= conf_lvl): rules.append((Y, X, conf_y_x))
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ df = pd.read_excel(
     skiprows=3, nrows=155
 )
 
-transactions = [
+transactions: list[set[str]] = [
     { item for item in df if df[item][transaction] == 1 }
     for transaction in range(1, 154)
 ]
 
 if __name__ == "__main__":
-    result = apriori(tuple(df), transactions, 0.5)
-    rules = get_rules(result, 0.75)
+    result: dict = apriori(tuple(df), transactions, 0.5)
+    rules: list[tuple[str, str, float]] = get_rules(result, 0.75)
 
     for x, y, conf in rules:
         print(f"{x}\t->\t{y}\t=\t{conf:.3f}")
